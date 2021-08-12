3bf1014c5e86f1c658f848b780c0c581fc1a76da
Luca Crupi <lucacrupi@MBP-di-Luca.home-life.hub>
2021-08-12 11:24:26 +0200

    merge fix

--- ScreenRecorder.cpp
+++ ScreenRecorder.cpp
@@ -134,7 +134,7 @@ int ScreenRecorder::initOutputFile() {
         cerr << "Error in creating AVFormatStream" << endl;
         exit(-6);
     }
-    
+
     outAVCodec = nullptr;   //avoid segmentation fault on call avcodec_alloc_context3(outAVCodec)
     outAVCodecContext = avcodec_alloc_context3(outAVCodec);
     if(outAVCodecContext == nullptr){
